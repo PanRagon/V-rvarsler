ea93ce194296b90f643993aaf3f4f9d8d9fd45d8
PanRagon <thebigecko@gmail.com>
2020-10-30 15:58:04 +0100

    Initial Commit

---
+++ Værvarsler/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Værvarsler
+//
+//  Created by Christian Nicolai Iversen on 30/10/2020.
+//  Copyright © 2020 Kandidatnummer 10042. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
